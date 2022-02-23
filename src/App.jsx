1aa31c8aa195035b08462071de9591639ba461da
josephmabusela <josephmabusela5@gmail.com>
2022-02-23 20:52:55 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/into/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
